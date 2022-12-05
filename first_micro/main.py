0ba1d37588d36c930300d23e335bc44c56729d79
FIRST_NAME LAST_NAME <titouan.kerneur@gmail.com>
2022-12-05 15:18:07 +0100

    third commit site change with GA

--- first_micro/main.py
+++ first_micro/main.py
@@ -4,4 +4,15 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-STW104EH1L"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ' G-STW104EH1L');
+    </script>
+    """
+    return prefix_google + "Hello World"
