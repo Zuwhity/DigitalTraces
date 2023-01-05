b74f8aaa3ad44819a737f85633492207c93e7fff
FIRST_NAME LAST_NAME <titouan.kerneur@gmail.com>
2023-01-05 17:12:41 +0100

    tp2/tp3

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # DigitalTraces
+
+During our class session at EPF we had to create a small web site via Deta App that host it for free and where we can do some analytics on it.
+We also need to had the dependencies of flask project
+
+The URL of the web site is the following : https://itsyzi.deta.dev
+
+You will also find the code I used to print the Hello World message and get the number of visitors getting on my website.
+
+
--- first_micro/main.py
+++ first_micro/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, url_for
 from markupsafe import escape # to escape user inputs and so avoid injection attacks
 import requests
+from pytrends.request import TrendReq
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
+
 
 app = Flask(__name__)
 
@@ -56,3 +61,59 @@ def cookieGA():
     req = requests.get("https://analytics.google.com/analytics/web/#/p345095181/reports/intelligenthome?params=_u..nav%3Dmaui")
 
     return req.text
+
+# @app.route('/visitors', methods = ["GET", "POST"])
+# def get_visitors(response):
+#   visitors = 0 # in case there are no analytics available yet
+#   for report in response.get('reports', []):
+#     columnHeader = report.get('columnHeader', {})
+#     metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+
+#     for row in report.get('data', {}).get('rows', []):
+#       dateRangeValues = row.get('metrics', [])
+
+#       for i, values in enumerate(dateRangeValues):
+#         for metricHeader, value in zip(metricHeaders, values.get('values')):
+#           visitors = value
+
+#   return str(visitors)
+def dataframe_to_list(df):
+    # Create an empty list
+    df_list = []
+
+    # Iterate through the rows
+    for _, row in df.iterrows():
+        # Get the values for each column and append them to the list
+        df_list.append([value for value in row])
+
+    return df_list
+
+@app.route('/chartpytrends', methods = ["GET", "POST"])
+def chartpytrend():
+    #pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), proxies=['https://34.203.233.13:80',], retries=2, backoff_factor=0.1, requests_args={'verify':False})
+
+    pytrends = TrendReq()
+    kw_list = ['geneve']
+    pytrends.build_payload(kw_list=kw_list, timeframe='today 5-y')
+    trend_data = pytrends.interest_over_time()
+
+    # Create a line chart using Matplotlib
+    plt.plot(trend_data['geneve'])
+    plt.xlabel('Date')
+    plt.ylabel('Trend')
+
+    # Save the chart to a PNG file
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    buf.seek(0)
+
+    # Encode the chart in base64
+    chart_url = base64.b64encode(buf.getvalue()).decode()
+    plt.clf()
+
+    # Render the chart in an HTML template
+    return render_template('plot.html', chart_url=chart_url)
+
+        
+
+
